128b3d54102bf8601c75685fdcbd4165bfd826c6
fikret2006 <Fikret.huseynov2006@gmail.com>
2022-11-21 19:00:53 +0400

    changed the code

--- assignment_21_nov/Program.cs
+++ assignment_21_nov/Program.cs
@@ -6,20 +6,27 @@
         {
             #region Calculator App
             Console.WriteLine("This is a calculator app!");
-            try
+            string startAgain = "yes";
+            while (startAgain =="yes")
             {
-                Console.WriteLine("Please, enter the first number:");
-                double num1 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Please, enter the second number:");
-                double num2 = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Please, enter the operator: (possible operators: + ; - ; * ; /)");
-                char operation = Convert.ToChar(Console.ReadLine());
-                Calculator(num1, num2, operation);
+                try
+                {
+                    Console.WriteLine("\nPlease, enter the first number:");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please, enter the second number:");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please, enter the operator: (possible operators: + ; - ; * ; /)");
+                    char operation = Convert.ToChar(Console.ReadLine());
+                    Calculator(num1, num2, operation);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occured! Error info: " + ex.Message);
+                }
+                Console.WriteLine("\nDo you want to continue working with the calculator? (yes/no)");
+                startAgain = Console.ReadLine();
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occured! Error info: " + ex.Message);
-            } 
+
             #endregion
         }
         static double Calculator(double num1, double num2, char operation)
